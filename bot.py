94f0bcbccb5066fd7a2f48de4ca514c712ea6efb
Chimaezss <cheesewithease@outlook.com>
2023-05-03 22:31:08 -0400

    added basic discord bot and mlb api

--- bot.py
+++ bot.py
@@ -1,16 +1,25 @@
 # bot.py
 import os
-
 import discord
+import mlbstatsapi
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+mlb = mlbstatsapi.Mlb()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print('Discord bot has connected!')
+
+@client.event
+async def on_message(message):
+    print(f'From {message.author}: {message.content}')
 
 client.run(TOKEN)

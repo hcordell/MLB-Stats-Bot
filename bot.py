bf60ea1d5c29359199562b7ffde5610c3fb6e10b
Chimaezss <cheesewithease@outlook.com>
2024-04-14 16:40:11 -0400

    fixed bug in saves

--- bot.py
+++ bot.py
@@ -137,9 +137,9 @@ def get_stats(mlb, gameID, player, playerID, position):
                 return None
     if summary:
         player_attributes[f'{player}']['Game ID'] = gameID
-        bsv = mlb.get_game_box_score(gameID).teams.away.players[f"id{playerID}"].stats['pitching']['blownsaves']
-        sv = mlb.get_game_box_score(gameID).teams.away.players[f"id{playerID}"].stats['pitching']['saves']
         if position == 'pitching' and get_game_finish(mlb, gameID):
+            bsv = mlb.get_game_box_score(gameID).teams.away.players[f"id{playerID}"].stats['pitching']['blownsaves']
+            sv = mlb.get_game_box_score(gameID).teams.away.players[f"id{playerID}"].stats['pitching']['saves']
             if bsv == 1:
                 summary += ', 1 BSV'
             elif sv == 1:
--- import mlbstatsapi.py
+++ import mlbstatsapi.py
@@ -5,7 +5,7 @@ mlb = mlbstatsapi.Mlb()
 player = mlb.get_people_id("Cristian Javier")
 schedule = mlb.get_scheduled_games_by_date(date.today())
 status = mlb.get_game(746404)
-summary = mlb.get_game_box_score(746404).teams.home.players[f"id{664299}"].stats['pitching']
+summary = mlb.get_game_box_score(746404).teams.home.players[f"id{664299}"].stats['pitching']['blownsaves']
 # Kenley Jansen GAME ID: 746977 PLAYER ID: 445276
 # Tyler O'Neill: GAME ID: 746249 PLAYER ID: 641933
 
